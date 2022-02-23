8329b18d2ad2663dd2a6c29d6eb346d2cb108e69
Keks <keks@htmlacademy.ru>
2022-02-23 14:44:29 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,13 @@
+const commentMaxLenght = 140;
+
+let commentCurrentLenght;
+
+
+function checkCommentLenght (commentCurrentLenght, commentMaxLenght) {
+  if (commentCurrentLenght <= commentMaxLenght) {
+    return: true;
+  }
+    return: false;
+}
+
+checkCommentLenght();
