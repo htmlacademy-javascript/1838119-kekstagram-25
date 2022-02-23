dd59f3ab1d8fa076a427a654d106b3f9c2fe370b
Каролина <carolina16_kv@mail.ru>
2022-02-23 23:07:37 +1000

    правки

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-const commentMaxLenght = 140;
-
-let commentCurrentLenght;
-
-
-function checkCommentLenght (commentCurrentLenght, commentMaxLenght) {
-  if (commentCurrentLenght <= commentMaxLenght) {
-    return: true;
-  }
-    return: false;
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-checkCommentLenght();
+//взяла из https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#получение_случайного_целого_числа_в_заданном_интервале_включительно
+
+function checkStringLength (str, stringLength ) {
+  if (str.length <= stringLength) {
+    return true;
+  }
+    return false;
+}
+
+console.log(checkStringLength('hello world', 12));
+
+console.log(getRandomIntInclusive(10,50));
