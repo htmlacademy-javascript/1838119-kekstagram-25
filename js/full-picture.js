dd0b5ead3e15ab89dc30b29fc00440ff63d05d74
Каролина <carolina16_kv@mail.ru>
2022-04-06 13:25:37 +1000

    pravki

--- js/full-picture.js
+++ js/full-picture.js
@@ -1,22 +1,22 @@
 // Реализовать сценарий просмотра фотографий в полноразмерном режиме
 
-const openBigPicture = () => {
-  const pictureSmallSize = document.querySelectorAll('.picture');
-  const viewFullSizePictureWindow = document.querySelector('.big-picture');
+// const openBigPicture = () => {
+//   const pictureSmallSize = document.querySelectorAll('.picture');
+//   const viewFullSizePictureWindow = document.querySelector('.big-picture');
 
 
-  for (let i = 0; i < pictureSmallSize.length; i++) {
-    pictureSmallSize[i].addEventListener('click', ()=> {
-      viewFullSizePictureWindow.classList.remove('hidden');
-      const openPictureFullSize = function () {
-        document.querySelector('.big-picture__big-img').src = pictureSmallSize[i].src;
-        document.querySelector('.likes-count').textContent = pictureSmallSize[i].textContent;
-      };
-    });
-  }
+//   for (let i = 0; i < pictureSmallSize.length; i++) {
+//     pictureSmallSize[i].addEventListener('click', ()=> {
+//       viewFullSizePictureWindow.classList.remove('hidden');
+//       // const openPictureFullSize = function () {
+//       //   document.querySelector('.big-picture__big-img').src = pictureSmallSize[i].src;
+//       //   document.querySelector('.likes-count').textContent = pictureSmallSize[i].textContent;
+//       // };
+//     });
+//   }
 
-};
+// };
 
-export {openBigPicture};
+// export {openBigPicture};
 
 
